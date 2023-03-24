4ad6738ebefa9659546b3318fb668860ecfc8c54
lazyukn <aironik74@gmail.com>
2023-03-25 03:40:39 +0500

    init

---
+++ go.mod
@@ -0,0 +1,16 @@
+module example/liker
+
+go 1.20
+
+require (
+	github.com/ndrewnee/go-yamusic v1.1.0
+	github.com/rubyist/circuitbreaker v2.2.1+incompatible
+)
+
+require (
+	github.com/cenk/backoff v2.2.1+incompatible // indirect
+	github.com/facebookgo/clock v0.0.0-20150410010913-600d898af40a // indirect
+	github.com/peterbourgon/g2s v0.0.0-20170223122336-d4e7ad98afea // indirect
+)
+
+replace github.com/ndrewnee/go-yamusic => /home/nikita/go-yamusic-extended
---
+++ liker.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/ndrewnee/go-yamusic/yamusic"
+	"github.com/rubyist/circuitbreaker"
+	"log"
+	"net/http"
+	"time"
+)
+
+// https://github.com/MarshalX/yandex-music-api/discussions/513
+const token = "secret_token"
+const playlistId = 100
+const userId = 100
+
+func main() {
+	circuitClient := circuit.NewHTTPClient(time.Second*5, 10, nil)
+	client := yamusic.NewClient(
+		yamusic.HTTPClient(circuitClient),
+		yamusic.AccessToken(userId, token),
+	)
+
+	playlist, resp, err := client.Playlists().Get(context.Background(), client.UserID(), playlistId)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		log.Fatal("http status is not 200")
+	}
+	var trackIds []string
+	for i := 0; i < len(playlist.Result.Tracks); i++ {
+		track := playlist.Result.Tracks[i]
+		trackIds = append(trackIds, track.Track.ID)
+	}
+
+	like, resp, err := client.Likes().Like(context.Background(), "track", trackIds)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		log.Fatal("http status is not 200")
+	}
+	fmt.Println("Like: ", like.Result)
+
+}
